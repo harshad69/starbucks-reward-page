d9f776e6c146123ac2d4b0a2fbfab8e61a920e43
Harshad <rharshad26@gmail.com>
2022-01-11 11:08:00 +0530

    1

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ var sticky = header.offsetTop;
 var navbar = document.getElementById("container1");
 function myFunction() {
     if (window.scrollY >= sticky) {
-        header.classList.add("sticky");
         navbar.classList.add("sticky");
     }
     else {
         navbar.classList.add("sticky"); 
-        header.classList.remove("sticky");
     }
 }
