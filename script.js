02a25e68f37e24789669e3ba36f56f895dd093ea
Harshad <rharshad26@gmail.com>
2022-02-15 22:25:39 +0530

    1

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("container2");
-var sticky = header.offsetTop;
-var navbar = document.getElementById("container1");
-function myFunction() {
-    if (window.scrollY >= sticky) {
-        navbar.classList.add("sticky");
-    }
-    else {
-        navbar.classList.add("sticky"); 
-    }
-}
+// var header = document.getElementById("container2");
+// var sticky = header.offsetTop;
+// var navbar = document.getElementById("container1");
+// function myFunction() {
+//     if (window.scrollY >= sticky) {
+//         navbar.classList.add("sticky");
+//     }
+//     else {
+//         navbar.classList.add("sticky"); 
+//     }
+// }
 
 // get your fav for free
 var divs = ["div1","div2","div3","div4","div5"];
